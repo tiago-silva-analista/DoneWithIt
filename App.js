c8975fc88ee5ed1bad62b3e2d7661e41766946fc
tiago-silva-analista <tiago.silva.analista@gmail.com>
2020-07-26 12:38:50 -0300

    Alert component from React Native

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image,TouchableWithoutFeedback, TouchableNativeFeedback, TouchableOpacity, TouchableHighlight, SafeAreaView } from 'react-native';
+import { StyleSheet, 
+          Text, 
+          View, 
+          Image,
+          TouchableWithoutFeedback, 
+          TouchableNativeFeedback, 
+          TouchableOpacity, 
+          TouchableHighlight, 
+          SafeAreaView,
+          Button,
+          Alert } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Text clicked");
@@ -8,8 +18,12 @@ export default function App() {
     
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress} >Hello React Native</Text>
+      <Button title="Click Me"
+              onPress={() => Alert.alert("My title","My message",(text) => console.log(text))
+                // [{text: "yes", onPress: () => console.log("Yes Pressed")},
+                // {text: "No", onPress: () => console.log('No pressed')}])
+              }></Button>  
       <TouchableNativeFeedback onPress={(() => console.log('Image tapped'))}>
-        
       <Image 
       blurRadius={0}
       fadeDuration={1001}
